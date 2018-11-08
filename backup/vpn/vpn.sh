da22629bdaf0894e8b2fe6aec35589bb21867b59
dave.zhao <dave.zhao@zerofinance.cn>
2018-11-08 16:26:08 +0800

    Mod 修改jenkins安装文档

--- backup/vpn/vpn.sh
+++ backup/vpn/vpn.sh
@@ -3,7 +3,7 @@
 set timeout -1
 #set PWD vagrant
 #spawn passwd
-spawn openconnect -u zhao --script=/etc/vpn/vpnc-script --no-dtls 1.1.1.1
+spawn openconnect -u dave.zhao --script=/etc/vpn/vpnc-script --no-dtls 14.118.132.74
 expect "确定"
 send "确定\r"
 expect "Password:"
